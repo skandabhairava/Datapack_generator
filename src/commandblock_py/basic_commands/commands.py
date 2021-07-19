0fa6d3d37babc0568d047a6d4aa8d95ad7575b4c
Anthony2be <adubovitsky1234@gmail.com>
2021-07-19 15:31:37 -0700

    add tellraw

--- src/commandblock_py/basic_commands/commands.py
+++ src/commandblock_py/basic_commands/commands.py
@@ -1,3 +1,5 @@
+from typing import List, Dict
+
 def say(context:str="") -> str :
     """
     context:str -> The text to be printed in chat
@@ -21,3 +23,11 @@ def kill(selector:str="") -> str:
     selector:str -> All the entities that must be killed
     """
     return f"kill {selector}\n"
+
+def tellraw(selector:str, nbt:Dict):
+    """
+    selector:str -> The thing to say the tellraw to
+    nbt:dict -> the json/nbt needed for the tellraw, might add ez nbt later
+    """
+    return f"tellraw {selector} {nbt}\n"
+    
