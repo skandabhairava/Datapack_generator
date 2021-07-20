3a59ac42c4f26c8c2c6f5f966eab259546fc2028
Skanda Bhairava <skandabhairava@gmail.com>
2021-07-20 13:43:00 +0530

    fixed issues

---
+++ cool_dasadtapack/data/asadsdsdbc/functions/tickkk.mcfunction
@@ -0,0 +1,2 @@
+say hi
+say well hello there
--- wrapper_test.py
+++ wrapper_test.py
@@ -1,9 +1,18 @@
 from src.commandblock_py.wrapper import *
 
-mypack = Datapack(datapack_name="cool_dasadtapack", namespace_id="asadsdsdbc", pack_version=7, datapack_description="My brand new datapack", loadjson="load", tickjson="tickkk")
+mypack = Datapack_wrap(datapack_name="cool_dasadtapack", namespace_id="asadsdsdbc", pack_version=7, datapack_description="My brand new datapack", loadjson="load", tickjson="tickkk")
 
-@mypack.new_function('pp')
+@mypack.new_function('load')
 def test(ctx):
     ctx.register('say hi')
+    #print("hello")
 
+@mypack.new_function('tickkk')
+def well(ctx):
+    ctx.register('say hi')
+    ctx.register('say well hello there')
+    #print("there")
+
+test()
+well()
 mypack.gen(zip=False)
