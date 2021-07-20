3f8b5fc77a228905b70d7b88b9655611c89b22e4
Anthony2be <adubovitsky1234@gmail.com>
2021-07-19 20:58:05 -0700

    make vars neater

---
+++ src/commandblock_py/autofill.py
@@ -0,0 +1,20 @@
+class _entities():
+    CREEPER = "minecraft:creeper"
+    SKELETON = "minecraft:skeleton"
+    SPIDER = "minecraft:spider"
+    ZOMBIE = "minecraft:zombie"
+    GHAST = "minecraft:ghast"
+    ENDER_DRAGON = "minecraft:ender_dragon"
+    ENDER_PEARL = "minecraft:ender_pearl"
+
+class _selectors():
+    NEAREST_PLAYER = "@p"
+    ALL_PLAYERS = "@a"
+    ALL_ENTITIES = "@e"
+    RANDOM_PLAYER = "@r"
+    CURRENT_ENTITY = "@s"
+    ALL_TARGET_SELECTORS = ["@p", "@a", "@e", "@r", "@s"]
+
+entities = _entities()
+
+selectors = _selectors()
--- tests.py
+++ tests.py
@@ -1,11 +1,12 @@
-from src.commandblock_py import Datapack
-from src.commandblock_py.basic_commands import commands, selectors, autofill
+from src.commandblock_py import Datapack, autofill
+from src.commandblock_py.basic_commands import commands, selectors
 
 mypack = Datapack(datapack_name="cool_dasadtapack", namespace_id="asadsdsdbc", pack_version=7, datapack_description="My brand new datapack", loadjson="load", tickjson="tickkk")
 print(mypack)
 #mypack.register_function(name="load",content=["say hi!"])
 mypack.register_function(name="pp",content="say hi!")
-mypack.register_function(name="tickkk",content=[commands.kill(selectors.selector(autofill.ALL_ENTITIES, type=autofill.CREEPER, etcetera_args="limit=5, sort=nearest"))])
+mypack.register_function(name="tickkk",content=[commands.kill(selectors.selector(autofill.selectors.ALL_ENTITIES, type=autofill.entities.CREEPER, etcetera_args="limit=5, sort=nearest"))])
 mypack.register_function(name="load",content=[commands.schedule('function','asadsdsdbc:pp','100t')])
 mypack.register_function(name="test",content=[commands.tellraw(selector='@a',nbt=[{"text": "[PP]: ","color": "aqua"},{"text": "Successfully reloaded!","color": "dark_aqua"}])])
+mypack.register_function(name='toopee', content=[commands.tp(selector=autofill.selectors.ALL_ENTITIES, selector2=('1','-100','10'))])
 mypack.generate(zip=False)
